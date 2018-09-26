8dd4fd20243a9066cc470443d5fd061a9a638b35
EC2 Default User <pranaydhariwal07@gmail.com>
2018-09-26 23:21:33 +0000

    part1 completed

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,12 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  
+  def sort_movie_title()
+     link_to "Movie Title",{column: "movie_names"}, :id => "title_header"
+  end
+  
+  def sort_release_date()
+    link_to "Release Date",{column: "rel_date"}, :id => "release_date_header"
+  end
 end
