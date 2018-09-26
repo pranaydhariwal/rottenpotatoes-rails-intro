338b4647c4110347ea0d5443c33e66615d9700a2
EC2 Default User <pranaydhariwal07@gmail.com>
2018-09-26 23:15:09 +0000

    heroku part1

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,12 +3,4 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
-  
-  def sort_movie_title()
-     link_to "Movie Title", {colm: "title"}, :id => "title_header"
-  end
-  
-  def sort_release_date()
-    link_to "Release Date", {colm: "date"}, :id => "date_header"
-  end
 end
