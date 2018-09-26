218452ae33a998bf5a735be533b46883b6149ff1
EC2 Default User <pranaydhariwal07@gmail.com>
2018-09-26 23:49:49 +0000

    part2 completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,35 +11,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-     @all_ratings = Movie.uniq.pluck(:rating)
-    if params[:ratings] == nil
-      @boxes = []
-    else 
-      @boxes = params[:ratings]
-    end
-    if params[:colm] == "movie"
-      @movies = Movie.order(:title).all
-      if params[:ratings].blank?
-        @movies = Movie.order(:title).all
-      else
-        @movies = Movie.order(:title).select {|i| @boxes.include?(i.rating)?true:false}
-      end
-      @css_valid = 1
-    elsif params[:colm] == "releasedate"
+    if params[:column] == "rel_date"
       @movies = Movie.order(:release_date).all
-      if params[:ratings].blank?
-        @movies = Movie.order(:release_date).all
-      else
-        @movies = Movie.order(:release_date).select {|i| @boxes.include?(i.rating)?true:false}
-      end
       @css_valid = 0
+    elsif params[:column] == "movie_names"
+      @movies = Movie.order(:title).all
+      @css_valid = 1
     else 
       @movies = Movie.all
-      if not params[:ratings].blank?
-        @movies = Movie.all.select {|i| @boxes.include?(i.rating)?true:false}
-      else 
-        @movies = Movie.all
-      end
+    end
   end
 
   def new
